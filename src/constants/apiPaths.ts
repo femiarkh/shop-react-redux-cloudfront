f89e2a55e68f202e61177d620c03535db5662fc9
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-10-11 22:45:23 +0300

    feat: get token from LS and put it to Authorization header

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://sbpgk9y44e.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://g2r4l47224.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://g2r4l47224.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://sbpgk9y44e.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
